33c9551f8de03a10151c0de310d5a50b21e3b9f3
B Flavia <b.flavia.ioana@gmail.com>
2022-08-16 14:37:55 +0300

    Refactor code for accordion to improve reusability

--- HamburgerMenu/menu.js
+++ HamburgerMenu/menu.js
@@ -61,7 +61,7 @@ document.onclick = (event) => {
 const addItemExpandIcon = (parentEl, iconSrc) => {
   const expandIconEl = document.createElement('img');
   expandIconEl.classList.add('open-close-icon');
-  expandIconEl.src = iconSrc;
+  expandIconEl.src = `assets/menu-icons/${iconSrc}`;
   expandIconEl.width = 35;
   expandIconEl.height = 35;
   expandIconEl.classList.add('ml-auto');
@@ -73,58 +73,67 @@ const getSubitems = (subitems, depth = 1) => {
   const subitemsContainer = document.createElement('div');
   subitemsContainer.classList.add('column', 'd-none');
   subitems.forEach((item) => {
-    const itemEl = document.createElement('div');
-    itemEl.classList.add('item', 'row', 'align-center');
-    const itemLabelEl = document.createTextNode(item.label);
-    itemEl.appendChild(itemLabelEl);
+    let itemEl = getItemElementContainer();
+    itemEl = appendLabelToItem(itemEl, item.label);
     itemEl.classList.add(`menu-depth-${depth}`);
     subitemsContainer.appendChild(itemEl);
+
     if (item.subitems?.length) {
-      const openArrow = addItemExpandIcon(
-        itemEl,
-        'assets/menu-icons/open-accordion.svg'
-      );
-      const closeArrow = addItemExpandIcon(
-        itemEl,
-        'assets/menu-icons/close-accordion.svg'
-      );
-      closeArrow?.classList.add('d-none');
-      let subitemsEl = getSubitems(item.subitems, depth + 1);
-      itemEl.addEventListener('click', () => {
-        toggleSubmenu(subitemsEl);
-        openArrow?.classList.toggle('d-none');
-        closeArrow?.classList.toggle('d-none');
-      });
+      const subitemsEl = getItemOpenCloseArrows(itemEl, item, depth + 1);
       subitemsContainer.appendChild(subitemsEl);
     }
   });
   return subitemsContainer;
 };
 
-const toggleSubmenu = (elementToToggle) =>
-  elementToToggle?.classList.toggle('d-none');
-menuItems.forEach((item) => {
+const getItemOpenCloseArrows = (itemEl, item, depth) => {
+  const arrowIconSufix = depth ? '' : '-white';
+  const openArrow = addItemExpandIcon(
+    itemEl,
+    `open-accordion${arrowIconSufix}.svg`
+  );
+  const closeArrow = addItemExpandIcon(
+    itemEl,
+    `close-accordion${arrowIconSufix}.svg`
+  );
+  closeArrow?.classList.add('d-none');
+  const subitemsEl = getSubitems(item.subitems, depth);
+  itemEl.addEventListener('click', () => {
+    toggleSubmenu(subitemsEl);
+    openArrow?.classList.toggle('d-none');
+    closeArrow?.classList.toggle('d-none');
+  });
+  return subitemsEl;
+};
+
+const getItemElementContainer = () => {
   const itemEl = document.createElement('div');
   itemEl.classList.add('item', 'row', 'align-center');
+  return itemEl;
+};
+
+const appendLabelToItem = (itemEl, label) => {
+  const itemLabelEl = document.createTextNode(label);
+  itemEl.appendChild(itemLabelEl);
+  return itemEl;
+};
+const getItemElement = (item) => {
+  let itemEl = getItemElementContainer();
   const itemImgEl = document.createElement('img');
   itemImgEl.src = item.imageSrc;
   itemEl.appendChild(itemImgEl);
-  const itemLabelEl = document.createTextNode(item.label);
-  itemEl.appendChild(itemLabelEl);
+  itemEl = appendLabelToItem(itemEl, item.label);
+  return itemEl;
+};
+
+const toggleSubmenu = (elementToToggle) =>
+  elementToToggle?.classList.toggle('d-none');
+
+menuItems.forEach((item) => {
+  const itemEl = getItemElement(item);
   let subitemsContainer;
   if (item.subitems?.length) {
-    const openArrow = addItemExpandIcon(itemEl, 'assets/menu-icons/open-accordion-white.svg');
-    const closeArrow = addItemExpandIcon(
-      itemEl,
-      'assets/menu-icons/close-accordion.svg'
-    );
-    closeArrow?.classList.add('d-none');
-    subitemsContainer = getSubitems(item.subitems);
-    itemEl.addEventListener('click', () => {
-      toggleSubmenu(subitemsContainer);
-      openArrow?.classList.toggle('d-none');
-      closeArrow?.classList.toggle('d-none');
-    });
+    subitemsContainer = getItemOpenCloseArrows(itemEl, item);
   }
 
   menuItemsEl.appendChild(itemEl);
