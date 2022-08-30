894f1184c78eb77f59c2bf577c6d70b657eaae41
B Flavia <b.flavia.ioana@gmail.com>
2022-08-30 12:45:15 +0300

    Add new folder for reviews carousell.

---
+++ ReviewsCarousell/reviews.js
@@ -0,0 +1,29 @@
+const reviewContainer = document.getElementById('reviews-carousell');
+const reviewsArray = [
+  {
+    avatar: 'assets/review-icons/avatar1.jpg',
+    score: 5,
+    date: 'August, 27, 2020',
+    comment: 'Best clinic ever!',
+  },
+  {
+    avatar: 'assets/review-icons/avatar2.jpg',
+    score: 4.5,
+    date: 'October, 3, 2021',
+    comment: 'Nice staff!',
+  },
+  {
+    avatar: 'assets/review-icons/avatar3.jpg',
+    score: 3.5,
+    date: 'March, 7, 2022',
+    comment: 'Keep up the good work!',
+  },
+  {
+    avatar: 'assets/review-icons/avatar4.jpg',
+    score: 4,
+    date: 'August, 30, 2022',
+    comment: 'Awweee!',
+  },
+];
+
+
